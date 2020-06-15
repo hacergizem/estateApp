5a03d0dc04c7591c667a0cf7ee44c37cc2c469bf
Gizem GOK <gizemgk654@gmail.com>
2020-06-16 02:05:40 +0300

    Update eklendi, bug var

--- app/src/main/java/com/example/afinal/CustomAdapter.java
+++ app/src/main/java/com/example/afinal/CustomAdapter.java
@@ -1,9 +1,12 @@
 package com.example.afinal;
 
 import android.content.Context;
+import android.content.Intent;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -39,11 +42,24 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
+
         holder.input_id.setText(String.valueOf(input_id.get(position)));
         holder.input_title.setText(String.valueOf(input_title.get(position)));
         holder.input_town.setText(String.valueOf(input_town.get(position)));
         holder.input_pager.setText(String.valueOf(input_pager.get(position)));
+        holder.mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context,UpdateActivity.class);
+                intent.putExtra("id", String.valueOf(input_id.get(position)));
+                intent.putExtra("title", String.valueOf(input_title.get(position)));
+                intent.putExtra("town", String.valueOf(input_town.get(position)));
+                intent.putExtra("pager", String.valueOf(input_pager.get(position)));
+
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -54,6 +70,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
         TextView input_id, input_title, input_town, input_pager;
+        LinearLayout mainLayout;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -61,6 +78,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
             input_title = itemView.findViewById(R.id.textStatus);
             input_town = itemView.findViewById(R.id.textCity);
             input_pager = itemView.findViewById(R.id.textAdress);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
 
         }
     }
---
+++ app/src/main/java/com/example/afinal/UpdateActivity.java
@@ -0,0 +1,52 @@
+package com.example.afinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class UpdateActivity extends AppCompatActivity {
+
+    EditText title_input, town_input, pager_input;
+    Button update_button;
+    String id, title, town, pager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+
+        title_input = findViewById(R.id.addTitle2);
+        town_input = findViewById(R.id.addTown2);
+        pager_input = findViewById(R.id.addPager2);
+        update_button = findViewById(R.id.updateButton);
+
+        update_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        getAndSetIntentData();
+
+    }
+
+    void getAndSetIntentData(){
+        if (getIntent().hasExtra("id") && getIntent().hasExtra("title") && getIntent().hasExtra("town") && getIntent().hasExtra("pager")){
+            id = getIntent().getStringExtra("id");
+            title = getIntent().getStringExtra("title");
+            town = getIntent().getStringExtra("town");
+            pager = getIntent().getStringExtra("pager");
+
+            title_input.setText(title);
+            town_input.setText(town);
+            pager_input.setText(pager);
+        }
+        else{
+            Toast.makeText(this, "No Data", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
